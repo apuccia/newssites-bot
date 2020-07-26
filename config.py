f49be355eeb2df1f282e7f11bbd6781e2a562342
apuccia <alessandropuccia97@gmail.com>
2020-07-26 22:32:28 +0200

    added config file + added constants + added commandline argument handling

---
+++ config.py
@@ -0,0 +1,6 @@
+TELEGRAPH_KEY = "your telegra.ph api key"
+TELEGRAM_KEY = "your telegram api key"
+
+TELEGRAPH_ARTICLE_DATE_FORMAT = "%H:%M"
+PARSER = "html.parser"
+TIMESPAN = 120
--- rtm.py
+++ rtm.py
@@ -1,66 +1,102 @@
+import sys
+import getopt
 import feedparser
 import requests
 import time
 import pytz
+
+import config
 
 from bs4 import BeautifulSoup
 from datetime import datetime, timezone
 from telegram.ext import Updater, CommandHandler
 from telegraph import Telegraph
 
-LOCAL_TZ = pytz.timezone("Europe/Rome")
-UTC_TZ = pytz.timezone("Etc/UTC")
+TZ_LOCAL = pytz.timezone("Europe/Rome")
+TZ_UTC = pytz.timezone("Etc/UTC")
 
-def main():
-    telegraph = Telegraph("your telegra.ph key")
+TELEGRAPH = "https://telegra.ph/"
+NEWSSITES_SUPPORTED = ["RTM"]
+
+E_BAD_INPUT = "Usage: python " + sys.argv[0] + " -s \"news site name\""
+E_NOT_SUPPORTED = "News site not supported"
+
+def main(news_site):
+    telegraph = Telegraph(config.TELEGRAPH_KEY)
     
-    print(telegraph.get_account_info())
-    last_article_date = UTC_TZ.localize(datetime.now())
+    last_article_date = TZ_UTC.localize(datetime.now())
+    aux = None
 
     while True:
-        feed = feedparser.parse("http://www.radiortm.it/feed/")
+        feed = feedparser.parse(news_site[2])
 
         for entry in feed.entries:
             article_date = datetime.strptime(entry.published, '%a, %d %b %Y %H:%M:%S %z')
             
             if last_article_date < article_date:
-                last_article_date = article_date
+                if entry == feed.entries[0]:
+                    aux = article_date
 
-                local_date = article_date.astimezone(LOCAL_TZ).strftime('%H:%M')
-                
-                print(entry.link)
+                art_local_date = article_date.astimezone(TZ_LOCAL).strftime(config.TELEGRAPH_ARTICLE_DATE_FORMAT)
 
+                print(entry.link) 
                 request = requests.get(entry.link)
-                html_content = request.content
                 
-                soup = BeautifulSoup(html_content, 'html5lib')
-                news = soup.find('div', class_='elementor-element elementor-element-6553dd6d contenuto_post elementor-widget elementor-widget-theme-post-content')
-
-                if news != None:
-                    content = "<p>" + local_date + "</p>"
-
-                    for par in news.find_all(find_filter):
-                        child = par.find("span")
-
-                        if (child != None):
-                            child.unwrap()
-
-                        content += str(par)
-
-                    content += "<p>Fonte: RTM (<a href=\"" + entry.link + "\">Leggi l'articolo e i commenti degli utenti sul sito di RTM</a>)</p>"
-
-                    author = "RTM-" + entry.author
-                    link = telegraph.create_page(entry.title, html_content = content, author_name = author)
-                    print('https://telegra.ph/{}'.format(link['path']))
-                else:
-                    print("Class attribute changed!")
+                html_content = request.content
+                soup_for_image = BeautifulSoup(html_content, config.PARSER)
+                soup_for_content = BeautifulSoup(entry.content[0].value, config.PARSER)
+        
+                article_content = article_generator(news_site, soup_for_image, soup_for_content, art_local_date, entry.link)
+                
+                author = news_site[0] + " " + entry.author
+                telegraph_link = telegraph.create_page(entry.title, html_content = article_content, author_name = author, author_url = news_site[1])
+                print(TELEGRAPH + '{}'.format(telegraph_link['path']))
             else:
                 break
+        
+        if aux != None:
+            last_article_date = aux
 
-        time.sleep(120)
+        time.sleep(config.TIMESPAN)
 
-def find_filter(tag):
-    return tag.name == "p" or (tag.name == "a" and tag.has_attr("href") and tag.has_attr("class"))
+def article_generator(news_site, img_soup, content_soup, art_date, art_link):
+    article_content = "<p>" + art_date + "</p>"
+
+    image = img_soup.find('a', class_ = 'post_thumb')
+    article_content += str(image)
+                    
+    for par in content_soup.find_all('p'):
+        child = par.find("span")
+
+        if (child != None):
+            child.unwrap()
+
+        article_content += str(par)
+                
+        article_content += "<p>Fonte: " + news_site[0] + " (<a href=\"" + art_link + "\">Leggi l'articolo e i commenti degli utenti sul sito di " + news_site[0] + "</a>)</p>"
+
+    return article_content
+
+def get_news_site(x):
+    return {
+        "RTM" : ["RTM", "https://www.radiortm.it/", "http://www.radiortm.it/feed/"]
+    }
 
 if __name__ == '__main__':
-    main()        
+    if len(sys.argv) != 3:
+        print(E_BAD_INPUT)
+        sys.exit(1)
+
+    try:
+        (opt, arg) = getopt.getopt(sys.argv[1:], "s:", "-newssite=")  
+    except getopt.GetoptError:
+        print(E_BAD_INPUT)
+        sys.exit(1) 
+
+    if arg not in NEWSSITES_SUPPORTED:
+        print(E_NOT_SUPPORTED)
+        sys.exit(1)
+
+    news_site = get_news_site(arg)
+
+    main(news_site)        
