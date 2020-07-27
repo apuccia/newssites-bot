bcfdb933f5aa8e49eb5d439d305db33ad26f684d
apuccia <alessandropuccia97@gmail.com>
2020-07-27 21:34:45 +0200

    added newssites json + some exception/error handling

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 TELEGRAPH_KEY = "your telegra.ph api key"
 TELEGRAM_KEY = "your telegram api key"
 
-TELEGRAPH_ARTICLE_DATE_FORMAT = "%H:%M"
+TELEGRAPH_ARTICLE_DATE_FORMAT = "%a, %d %b %Y %H:%M"
 PARSER = "html.parser"
-TIMESPAN = 120
+FEED_TIMESPAN = 120
+CONNECTION_TIMESPAN = 300
+NEWS_SITES = "./newssites.json"
--- rtm.py
+++ rtm.py
@@ -4,6 +4,7 @@ import feedparser
 import requests
 import time
 import pytz
+import json
 
 import config
 
@@ -13,51 +14,81 @@ from telegram.ext import Updater, CommandHandler
 from telegraph import Telegraph
 
 TZ_LOCAL = pytz.timezone("Europe/Rome")
-TZ_UTC = pytz.timezone("Etc/UTC")
 
 TELEGRAPH = "https://telegra.ph/"
-NEWSSITES_SUPPORTED = ["RTM"]
 
-E_BAD_INPUT = "Usage: python " + sys.argv[0] + " -s \"news site name\""
-E_NOT_SUPPORTED = "News site not supported"
+USAGE = "Usage: python " + sys.argv[0] + " -s \"news site name\""
 
-def main(news_site):
+E_BAD_INPUT = "Wrong option or no argument inserted"
+E_CONNECTION = "Connection error, retrying in " + str(config.CONNECTION_TIMESPAN)
+E_PERMANENT_REDIRECT = "Feed permanently redirected, modify config file"
+E_FEED_NOT_AVAILABLE = "Feed not available"
+
+def main(news_sites):
     telegraph = Telegraph(config.TELEGRAPH_KEY)
     
-    last_article_date = TZ_UTC.localize(datetime.now())
+    last_article_date = None
     aux = None
 
     while True:
-        feed = feedparser.parse(news_site[2])
+        for news_site in news_sites:
+            feed = feedparser.parse(news_site.get("rss"))
+            if feed.status == None:
+                print(E_CONNECTION)
+                time.sleep(config.CONNECTION_TIMESPAN)
+                continue
+            elif feed.status == 301:
+                print(E_PERMANENT_REDIRECT)
+                news_sites.get(news_site)["rss"] = feed.href
+                print("Feed URL changed, json file modified with the new URL: " + feed.href)
+            elif feed.status == 410:
+                print(E_FEED_NOT_AVAILABLE)
 
-        for entry in feed.entries:
-            article_date = datetime.strptime(entry.published, '%a, %d %b %Y %H:%M:%S %z')
+            iterator = iter(feed.entries)
+            entry = next(iterator, None)
+            while entry != None:
+                article_date = datetime.strptime(entry.published, '%a, %d %b %Y %H:%M:%S %z')
+                print(article_date)
+                if last_article_date == None:
+                    last_article_date = article_date
+
+                if last_article_date < article_date:
+                    try:
+                        request = requests.get(entry.link)
+                    except requests.exceptions.ConnectionError:
+                        print(E_CONNECTION)
+                        time.sleep(config.CONNECTION_TIMESPAN)
+                        continue
+
+                    print("--A new article is out!")
+
+                    if entry == feed.entries[0]:
+                        aux = article_date
+
+                    art_local_date = article_date.astimezone(TZ_LOCAL).strftime(config.TELEGRAPH_ARTICLE_DATE_FORMAT)
+                    print("\tArticle local date: " + art_local_date)
+
+                    print("\tArticle link: " + entry.link) 
+
+                    html_content = request.content
+                    soup_for_image = BeautifulSoup(html_content, config.PARSER)
+                    soup_for_content = BeautifulSoup(entry.content[0].value, config.PARSER)
             
-            if last_article_date < article_date:
-                if entry == feed.entries[0]:
-                    aux = article_date
+                    article_content = article_generator(news_site, soup_for_image, soup_for_content, art_local_date, entry.link)
+                    
+                    author = news_site.get("name") + " " + entry.author
+                    telegraph_link = telegraph.create_page(entry.title, html_content = article_content, author_name = author, author_url = news_site.get("website"))
+                    print("\tTelegra.ph link: " + TELEGRAPH + '{}'.format(telegraph_link['path']))
+                else:
+                    break
 
-                art_local_date = article_date.astimezone(TZ_LOCAL).strftime(config.TELEGRAPH_ARTICLE_DATE_FORMAT)
+                entry = next(iterator, None)
+            
+            if aux != None:
+                last_article_date = aux
 
-                print(entry.link) 
-                request = requests.get(entry.link)
-                
-                html_content = request.content
-                soup_for_image = BeautifulSoup(html_content, config.PARSER)
-                soup_for_content = BeautifulSoup(entry.content[0].value, config.PARSER)
-        
-                article_content = article_generator(news_site, soup_for_image, soup_for_content, art_local_date, entry.link)
-                
-                author = news_site[0] + " " + entry.author
-                telegraph_link = telegraph.create_page(entry.title, html_content = article_content, author_name = author, author_url = news_site[1])
-                print(TELEGRAPH + '{}'.format(telegraph_link['path']))
-            else:
-                break
-        
-        if aux != None:
-            last_article_date = aux
+        time.sleep(config.FEED_TIMESPAN)
 
-        time.sleep(config.TIMESPAN)
 
 def article_generator(news_site, img_soup, content_soup, art_date, art_link):
     article_content = "<p>" + art_date + "</p>"
@@ -73,30 +104,24 @@ def article_generator(news_site, img_soup, content_soup, art_date, art_link):
 
         article_content += str(par)
                 
-        article_content += "<p>Fonte: " + news_site[0] + " (<a href=\"" + art_link + "\">Leggi l'articolo e i commenti degli utenti sul sito di " + news_site[0] + "</a>)</p>"
+    article_content += "<p>Fonte: " + news_site.get("name") + " (<a href=\"" + art_link + "\">Leggi l'articolo e i commenti degli utenti sul sito di " + news_site.get("name") + "</a>)</p>"
 
     return article_content
 
-def get_news_site(x):
-    return {
-        "RTM" : ["RTM", "https://www.radiortm.it/", "http://www.radiortm.it/feed/"]
-    }
 
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print(E_BAD_INPUT)
-        sys.exit(1)
-
     try:
-        (opt, arg) = getopt.getopt(sys.argv[1:], "s:", "-newssite=")  
+        (opts, args) = getopt.getopt(sys.argv[1:], "hs:", ["help", "newssite="])  
     except getopt.GetoptError:
         print(E_BAD_INPUT)
         sys.exit(1) 
 
-    if arg not in NEWSSITES_SUPPORTED:
-        print(E_NOT_SUPPORTED)
-        sys.exit(1)
+    for opt, arg in opts:
+        if opt in ('-h', '--help'):
+            print(USAGE)
+            sys.exit(1)
+    
+    with open(config.NEWS_SITES, 'r') as jsonfile:
+        news_sites = json.load(jsonfile)   
 
-    news_site = get_news_site(arg)
-
-    main(news_site)        
+    main(news_sites)        
